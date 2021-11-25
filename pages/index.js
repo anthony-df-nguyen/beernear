b1c722d2e53f1af9d931f980b5caf24181e418ca
skifreetony <skifreetony@yahoo.com>
2021-11-24 18:03:16 -0800

    Placeholder search results box example implemented

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from "next/head";
+import React, { useState } from "react";
+import SearchResultsBox from "../components/SearchResultsBox";
 
 export default function Home() {
+
+  //Set a state that represents an array of the breweries fetched, initially starts out as an empty array
+  const fakeBreweryList = [{name: 'Beer Me Bitch'},{name:'Black Out'},{name:'White Girl Wasted'}]
+  const [fetchedBreweries, updateFetchedBreweries] = useState([]);
+
   return (
     <div className="">
       <Head>
@@ -13,6 +20,29 @@ export default function Home() {
           name="keywords"
           content="beer,breweries,near,me,united,states,openbrewerydb"></meta>
       </Head>
+
+      {/* Navigation Bar Here */}
+      <div className="pt-8 pb-8 bg-blue-200 text-center">
+        Navigation bar here
+      </div>
+
+      <div className="container mx-auto">
+        {/* Site Title Logo Here */}
+        <div className="my-4 pt-32 pb-32 h-auto bg-gray-50">
+          <div className="text-center">Site Title and Logo here</div>
+        </div>
+
+        {/* State/City Selector UI here */}
+        <div className="my-4 p-4 bg-blue-200 text-center">
+          City/State Dropdown Component Here
+        </div>
+
+        {/* List of Breweries Returned from the Search Here */}
+        <div className="my-4">
+          {/* We pass down the fetched breweries state/array into this search results component */}
+          <SearchResultsBox fetchedBreweries={fetchedBreweries} />
+        </div>
+      </div>
     </div>
   );
 }
