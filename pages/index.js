78b5db36ce6b8d31a2808409b8dcdebb95ab8a42
skifreetony <skifreetony@yahoo.com>
2021-11-24 17:13:24 -0800

    Added head component to main home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="bg-gray-400 p-4">
-      <div className="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-gray-50 p-2">Grid item 1</div>
-        <div className="bg-gray-50 p-2">Grid item 2</div>
-        <div className="bg-gray-50 p-2">Grid item 3</div>
-        <div className="bg-gray-50 p-2">Grid item 4</div>
-        <div className="bg-gray-50 p-2">Grid item 5</div>
-        <div className="bg-gray-50 p-2">Grid item 6</div>
-      </div>
+    <div className="">
+      <Head>
+        <title>Beer Near</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Simple website to find breweries near you in the United States utilizing the openbrewerydb API"></meta>
+        <meta
+          name="keywords"
+          content="beer,breweries,near,me,united,states,openbrewerydb"></meta>
+      </Head>
     </div>
   );
 }
